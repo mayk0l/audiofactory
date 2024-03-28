8561f9f58d348dde83dd0d50e8287ffc14e5bd3e
mayk0l <maykol0219@gmail.com>
2024-03-27 23:00:05 -0300

    changed title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.title}>TechnoGrooveFactory</h1>
+      <h1 className={styles.title}>GrooveFactory</h1>
       <p className={styles.description}>
       Explore Techno DJ sets from around the world
       </p>
